be601231885fc8aead2f7cbba3ac345d6fe42e5d
Tushar Godase <tushargodase30@gmail.com>
2025-06-19 16:53:21 +0530

    Comparison operators

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,5 +1,5 @@
+/*
 let score = "88"
-
 console.log(typeof score); 
 
 let ValueInNumber = Number(score); // Convert to Number
@@ -15,6 +15,24 @@ console.log(ValueInBoolean); // true (non-empty string is truthy)
 // Note: Converting a string to a number will result in NaN (Not a Number) if the string is not a valid number
 // Example of NaN
 let invalidNumber = Number("abc"); // NaN
-console.log(invalidNumber); // NaN
+console.log(invalidNumber); // NaN (Not a Number)
 console.log(typeof invalidNumber); // "number" (but it's NaN, which is a special value in JS)   
+*/
 
+// --------------------Operations-------------------------------
+
+let value = 3
+let negValue  = -value
+// console.log(negValue); //Prints negative value
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "Tushar "
+let Str2 = "Godase"
+let Name =  5 +5 + Str2
+console.log(Name)
